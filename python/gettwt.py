6e2ef8ffe392b202659309d1176d0cf5d6de3a64
Ogawa <yohdesign0@gmail.com>
2023-02-03 14:45:12 +0900

    ここから始まる

---
+++ python/gettwt.py
@@ -0,0 +1,8 @@
+import tweepy
+
+print('Hello world!')
+
+#Twitterの認証
+consumer_key = 'VeaCyexExiyCJNuxG5CKLfMB9'
+consumer_secret = 'nFCaAdsbcE4MvQnkvJN3SMDJt3WEHSecNOuI3AFTNgOmwj2LJh'
+access_token_secret = 'AAAAAAAAAAAAAAAAAAAAAAFIlgEAAAAA1zDSP0gtmCCnbTb1O4VdrHiPG%2FM%3DsNfhKLCXAaPt4qzqtBzWvICZBvZMmo82Opfvl0kSQD1R9yURli'
